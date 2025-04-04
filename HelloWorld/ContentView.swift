25a3801ad7faff621f80ec768e0f4eaf1f09f003
Efraín <efrain.arenas@kuantik.mx>
2025-04-04 15:52:42 -0600

    another commit

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var counter:Int = 0
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
+                .rotationEffect(.degrees(Double(counter)))
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.cyan)
+            Text("Pinga!")
+                //.padding(.bottom,Double(counter))
+            Text("\(counter)")
+			Text("Aumentar")
+				.onTapGesture(perform: incrementWhenPressed)
+				.background(.blue)
+            Button(action: incrementWhenPressed){
+                Text("Aumentar")
+					
+            }.background(.blue)
+            Button(action: restart){
+                Text("Reiniciar")
+            }.background(.red)
         }
         .padding()
+		.onAppear{
+			incrementWhenPressed()
+		}
+		
+    }
+    
+    func increment()->Int{
+        
+        return counter + 100;
+    }
+    
+    func incrementWhenPressed(){
+        withAnimation {
+            counter = increment()
+        }
+        
+    }
+    
+    func restart(){
+        withAnimation{
+            counter = 0
+        }
     }
 }
 
